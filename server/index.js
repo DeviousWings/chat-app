7e0b13a664260114d7dc1adeaaefe638bc7634b7
DeviousWings <nksteele@byui.edu>
2023-03-29 19:18:34 -0400

    finished

--- server/index.js
+++ server/index.js
@@ -8,12 +8,41 @@ const PORT = process.env.PORT || 5000;
 
 require("dotenv").config();
 
+const accountSID = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSID = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require("twilio")(accountSID, authToken);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   res.send("Server running on port 5000");
+});
+
+app.post("/", (req, res) => {
+  const { message, user: sender, type, members } = req.body;
+
+  if (type === "message.new") {
+    members
+      .filter((member) => member.user_id !== sender.id)
+      .foreach(({ user }) => {
+        if (!user.online) {
+          twilioClient.messages
+            .create({
+              body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+              messagingServiceSID: messagingServiceSID,
+              to: user.phoneNumber,
+            })
+            .then(() => console.log("Message Sent!"))
+            .catch((err) => console.log(err));
+        }
+      });
+    res.status(200).send("Message Sent!");
+  }
+
+  return res.status(200).send("Not a new message request");
 });
 
 app.use("/auth", authRoutes);
